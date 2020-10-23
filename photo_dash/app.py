a190a62f1227f76d5d664e70f4b02af5fea48b25
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-10-23 00:41:56 -0700

    Remove moved code from PhotoDash class; make DashImg a dataclass

--- photo_dash/app.py
+++ photo_dash/app.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Any, Dict, List, Union
 
 import pendulum
@@ -28,21 +29,31 @@ class PhotoDash(Resource):
         module = request.form['module']
         title = request.form['title']
         sections = request.form['data']
-        y = 0
-        with Image.new('RGB', config.CANVAS) as im:
-            draw = ImageDraw.Draw(im)
-            title_size = 20
-            title_font = ImageFont.truetype(font=FONT, size=title_size)
-            draw.text((0, 0), title, fill='#FFFFFF', font=title_font)
-            y = next_y(y, title_size)
-            for section in sections:
-                pass
-            im.save(f'{module}.jpg')
+        img = DashImg(module, title, sections)
+        img.create()
         return 201
 
 
+@dataclass
 class DashImg:
-    """Represents a photo-dash image."""
+    """Represents a photo-dash image.
+
+    Attributes:
+        module (str): the name of the module the image represents
+        title (str): title of the image; goes at the top of the image
+        sections (SECTIONS): a response given to the endpoint
+        FONT (str): the name of the font to use for all text elements
+        TITLE_COLOR (str): color for the title on an image ('#FFFFF')
+        TITLE_SIZE (int): size of the title (20)
+        TITLE_FONT (ImageFont.FreeTypeFont): font for the title
+        SPACER (int): how much to space rows of elements
+
+    """
+
+    module: str
+    title: str
+    sections: SECTIONS
+    y: int = 0  # This should not be initialized.
 
     SPACER = 1
     FONT = 'DejaVuSans.ttf'
@@ -51,11 +62,7 @@ class DashImg:
     TITLE_SIZE = 20
     TITLE_FONT = ImageFont.truetype(font=FONT, size=TITLE_SIZE)
 
-    def __init__(self) -> None:
-        """Initialize parameters for DashImg."""
-        self.y = 0
-
-    def next_y(self, delta: int) -> None:
+    def _next_y(self, delta: int) -> None:
         """Get the next value for y given a delta.
 
         To prevent vertical clutter, a small padding value will also apply.
@@ -66,15 +73,20 @@ class DashImg:
         """
         self.y += delta + self.SPACER
 
-    def create(self, module: str, title: str, sections: SECTIONS) -> None:
+    def create(self) -> None:
         """Create a new image given parameters."""
         with Image.new('RGB', config.CANVAS) as im:
             draw = ImageDraw.Draw(im)
-            draw.text((0, 0), title, fill=self.TITLE_COLOR, font=self.TITLE_FONT)
-            self.next_y(self.title_size)
-            for section in sections:
+            draw.text(
+                (0, 0),
+                self.title,
+                fill=self.TITLE_COLOR,
+                font=self.TITLE_FONT
+                )
+            self._next_y(self.title_size)
+            for section in self.sections:
                 pass
-            im.save(f'{module}.jpg')
+            im.save(f'{self.module}.jpg')
 
 
 API.add_resource(PhotoDash, '/')
