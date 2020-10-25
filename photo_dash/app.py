00e69bd48fa5dffc0da08f810ba530eeefc8759e
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-10-25 04:18:32 -0700

    Well, there's no way to use request.form here.

--- photo_dash/app.py
+++ photo_dash/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 
-from photo_dash import image
+from photo_dash import config, image
 
 
 APP = Flask(__name__)
@@ -18,11 +18,14 @@ class PhotoDash(Resource):
             int: HTTP status code 201
 
         """
-        module = request.form['module']
-        title = request.form['title']
-        sections = request.form['sections']
+        config.LOGGER.info('Got a request')
+        r = request.get_json()
+        module = r['module']
+        title = r['title']
+        sections = r['sections']
         img = image.DashImg(module, title, sections)
         try:
+            config.LOGGER.info('Attempting image creation')
             img.create()
         except image.TooManySections:
             return 401
